ad595209e569aecc07fa82984ebf687918954d45
Tomi <tomasagustinbrizuela@gmial.com>
2024-12-08 19:55:24 -0300

    mejoras en la ui

--- src/components/pages/blog/Blog.jsx
+++ src/components/pages/blog/Blog.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable react/prop-types */
 import './BlogStyle.css';
 import BlogItemsContainer from "../../common/BlogItems/BlogItemsContainer"
+import { Skeleton } from '@mui/material';
 
-const Blog = ({ info }) => {
+const Blog = ({ info, isLoading }) => {
     return (
         <div>
             <div className="listEntries">
                 <h1>My writings & thoughts</h1>
+                {isLoading && <div className='blogItem'><Skeleton style={{ width: "235px" }} /></div>}
+                {isLoading && <div className='blogItem'><Skeleton style={{ width: "198px" }} /></div>}
+                {isLoading && <div className='blogItem'><Skeleton style={{ width: "159px" }} /></div>}
+                {isLoading && <div className='blogItem'><Skeleton style={{ width: "240px" }} /></div>}
                 {
                     info.map((post) => {
-                        return <BlogItemsContainer key={post.id} post={post} />
+                        return isLoading ? <Skeleton /> : <BlogItemsContainer key={post.id} post={post} />
                     })
                 }
             </div>
--- src/components/pages/blog/BlogContainer.jsx
+++ src/components/pages/blog/BlogContainer.jsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react"
 import Blog from "./Blog"
 
 const BlogContainer = () => {
+    const [isLoading, setLoading] = useState(true);
     const [posts, setPosts] = useState([]);
     useEffect(() => {
         let url = "https://portfolioback-815092256362.us-central1.run.app/api/posts";
         fetch(url)
             .then((res) => res.json())
-            .then((res) => setPosts(res.Data))
+            .then((res) => {
+                setPosts(res.Data)
+                setLoading(false)
+            })
 
     }, [])
     return (
         <>
-            <Blog info={posts} />
+            <Blog info={posts} isLoading={isLoading} />
         </>
     )
 }
